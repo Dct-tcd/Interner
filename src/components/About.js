e7f963b04b222eb64ab8bd8f80378ffc1c987d0d
Dev <tcd16112002@gmail.com>
2023-04-02 17:54:30 +0530

    Slight tinkering

--- src/components/About.js
+++ src/components/About.js
@@ -8,11 +8,15 @@ export default function About() {
       At Job Hunter We Post Regular updates on internships and job postings!
 Our vision is to empower the youth of our nation and make them Atmanirbhar . By providing them with all kinds of opportunities from various sectors, sharing knowledge and make them competent enough so that they could excel in any field.
     </p>
-    {/* <div style={{display:"flex",color:"voilet" justifyContent:"center"}}> */}
+    <div style={{display:"flex",color:"voilet" ,justifyContent:"center",marginBottom:"50px"}}> 
 
-    {/* Practise DSA Your Skills */}
+  <a style={{ color: "inherit",
+  textDecoration: "inherit" , color:"lightblue"}}
+  href="https://dsa-tracker-by-devtiwari.netlify.app/"
+  target="_blank"
+  >Practise Your  DSA Skills Here 👈</a>
 
-    {/* </div> */}
+     </div>
     </>
   )
 }
