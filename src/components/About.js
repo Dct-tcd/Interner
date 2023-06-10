154a44c3495beab870d7b43c604ed91c3424b227
Dev <tcd16112002@gmail.com>
2023-06-10 08:29:31 +0530

    Responsivity

--- src/components/About.js
+++ src/components/About.js
@@ -7,17 +7,17 @@ export default function About() {
       <h1 style={{textAlign:"center", padding:"2%",marginTop:"5%",color:"beige",fontSize:"large" }}>About us</h1>
     <div style={{display:"flex",justifyContent:"center"}}> 
         
-    <div id="diver" style={{textAlign:"center", paddingLeft:"10%",paddingRight:"10%",paddingTop:"3%",
-    paddingBottom:"3%",color:"cornflowerblue",fontSize:"large"}}>
+    <div className="diver" id="diver">
 
-      At Job Hunter We Post Regular updates on internships and job postings!
+      At Interner , We Post Regular updates on internships and Job postings in  software development field.
+
 Our vision is to empower the youth of our nation and make them Atmanirbhar . By providing them with all kinds of opportunities from various sectors, sharing knowledge and make them competent enough so that they could excel in any field.
     </div>
     </div>
     <div style={{display:"flex",color:"voilet" ,justifyContent:"center",marginBottom:"50px"}}> 
 
   <a style={{ color: "inherit",marginTop:"1%",
-  textDecoration: "inherit" , color:"lightblue"}}
+  textDecoration: "inherit" , color:"lightblue",alignContent:"center"}}
   href="https://dsa-tracker-by-devtiwari.netlify.app/"
   target="_blank"
   >Practise Your  DSA Skills Here 👈</a>
--- src/components/Descriptioner.js
+++ src/components/Descriptioner.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 export default function Descriptioner(props) {
   return (
-    // style = {{display:props.vis!=0?'initial':'none'}}
     <div>
     <br></br>
     <h6 style={{width:"100%",height:"100%",marginLeft:'2px',marginRight:'2px',marginBottom:'2px'}}>
